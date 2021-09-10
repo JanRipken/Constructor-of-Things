d89aa77c5dfb95546927c7950c96a7b8ae657e4f
JanRipken <72017165+JanRipken@users.noreply.github.com>
2021-09-10 14:40:50 +0200

    Login and regis
    beides einmal vorerst eingefügt

--- Constructor-of-Things/Constructor-of-Things/seiten/Login.xaml.cs
+++ Constructor-of-Things/Constructor-of-Things/seiten/Login.xaml.cs
@@ -24,5 +24,54 @@ namespace Constructor_of_Things
         {
             InitializeComponent();
         }
+
+        private void Window_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+            if (e.LeftButton == MouseButtonState.Pressed)
+                DragMove();
+        }
+
+        private void LOGIN_Click(object sender, RoutedEventArgs e)
+        {
+            seiten.main_page main_Page = new seiten.main_page();
+            main_Page.Show();
+            this.Close();
+        }
+
+        private void REGISTRIEREN_Click(object sender, RoutedEventArgs e)
+        {
+            seiten.registrierung registrierung = new seiten.registrierung();
+            registrierung.Show();
+            
+        }
+
+        private void SCHLISSEN_Click(object sender, RoutedEventArgs e)
+        {
+            Application.Current.Shutdown();
+        }
+
+        private void txtb_Username_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            if (txtb_Username.Text.Length == 0)
+            {
+                lbl_username.Visibility = Visibility.Visible;
+            }
+            else
+            {
+                lbl_username.Visibility = Visibility.Hidden;
+            }
+        }
+
+        private void txtb_Password_PasswordChanged(object sender, RoutedEventArgs e)
+        {
+            if (txtb_Password.Password.Length == 0)
+            {
+                lbl_Passwort.Visibility = Visibility.Visible;
+            }
+            else
+            {
+                lbl_Passwort.Visibility = Visibility.Hidden;
+            }
+        }
     }
 }
--- Constructor-of-Things/Constructor-of-Things/seiten/registrierung.xaml.cs
+++ Constructor-of-Things/Constructor-of-Things/seiten/registrierung.xaml.cs
@@ -23,5 +23,57 @@ namespace Constructor_of_Things.seiten
         {
             InitializeComponent();
         }
+
+        private void Window_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+            if (e.LeftButton == MouseButtonState.Pressed)
+                DragMove();
+        }
+
+        private void SCHLISSEN_Click(object sender, RoutedEventArgs e)
+        {
+            this.Close();
+        }
+
+        private void btn_registrieren_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void CB_Geschlecht_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            if (CB_Geschlecht.SelectedIndex == 0)
+            {
+
+            }
+
+            if (CB_Geschlecht.SelectedIndex == 1)
+            {
+
+            }
+        }
+
+        private void CB_Hochschule_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            if (CB_Hochschule.SelectedIndex == 0)
+            {
+
+            }
+
+            if (CB_Hochschule.SelectedIndex == 1)
+            {
+
+            }
+
+            if (CB_Hochschule.SelectedIndex == 2)
+            {
+
+            }
+        }
+
+        private void DP_Gebrtstag_SelectedDateChanged(object sender, SelectionChangedEventArgs e)
+        {
+
+        }
     }
 }
