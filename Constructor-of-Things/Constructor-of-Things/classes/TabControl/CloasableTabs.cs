68be45a5e6a10c68aa9ff2c61810a5bb6e78f381
JanRipken <72017165+JanRipken@users.noreply.github.com>
2021-09-11 18:28:13 +0200

    uc init

---
+++ Constructor-of-Things/Constructor-of-Things/classes/TabControl/CloasableTabs.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Windows.Controls;
+
+namespace Constructor_of_Things.classes
+{
+    public class CloasableTabs : TabItem
+    {
+
+    }
+
+  
+}
